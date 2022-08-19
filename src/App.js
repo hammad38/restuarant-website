bdb5c251772f03e5ccf077e59e205013f468b2be
obenstein <hammadsami119@gmail.com>
2022-08-20 00:06:58 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Restuarant from "./restuarent/Restuarant"
+
+const App = () => {
+  return (
+    <div>
+    <Restuarant />
+    </div>
+  )
+}
+
+export default App
---
+++ src/restuarent/MenuApi.js
@@ -0,0 +1,104 @@
+const Menu = [
+  {
+    id: 1,
+    image: "../images/magginoodles.jpg",
+    name: "maggi",
+    category: "evening",
+    price: "RS. 50",
+    description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+  {
+    id: 2,
+    image: "../images/allupakoida.jpg",
+    name: "Pakore",
+    category: "evening",
+    price: "RS. 100",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 3,
+    image: "../images/corn.jpg",
+    name: "corn",
+    category: "breakfast",
+    price: "RS. 40",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 4,
+    image: "../images/chola.jpg",
+    name: "cholay",
+    category: "lunch",
+    price: "RS. 50",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 5,
+    image: "../images/pizza.jpg",
+    name: "pizza",
+    category: "evening",
+    price: "RS. 200",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 6,
+    image: "../images/nonvegthali.jpg",
+    name: "Thali",
+    category: "dinner",
+    price: "250",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 7,
+    image: "../images/sweet.jpg",
+    name: "Sweets",
+    category: "dinner",
+    price: "500",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 8,
+    image: "../images/rajmarice.jpg",
+    name: "Dal Chawal",
+    category: "lunch",
+    price: "50",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 9,
+    image: "../images/samosa.jpg",
+    name: "Samaso",
+    category: "evening",
+    price: "25",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 10,
+    image: "../images/chickentikka.jpg",
+    name: "Tikka",
+    category: "dinner",
+    price: "",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 11,
+    image: "../images/zingerburger.jpg",
+    name: "Zinger Burger",
+    category: "dinner",
+    price: "250",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+];
+
+export default Menu;
